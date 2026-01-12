3e41fa77f35d5f5b4b6f42925e66f5982df5546b
Quynhnhu0203 <163087967+Quynhnhu0203@users.noreply.github.com>
2026-01-12 10:11:51 +0700

    add descripton@@

--- app/src/main/java/com/example/dormdeli/model/Notification.kt
+++ app/src/main/java/com/example/dormdeli/model/Notification.kt
@@ -2,7 +2,7 @@ package com.example.dormdeli.model
 
 data class Notification(
     val id: String = "",
-    var target: String = "",
+    var target: String = "", //id ng nhận
     val subject: String = "",
     val message: String = "",
     val createdAt: Long = System.currentTimeMillis()
