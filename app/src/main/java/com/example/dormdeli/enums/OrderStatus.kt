ba92df7d274883548d6deddf91ce0eb4b73d5299
Quynhnhu0203 <163087967+Quynhnhu0203@users.noreply.github.com>
2026-01-13 21:34:42 +0700

    order status: accepted, picked_up

--- app/src/main/java/com/example/dormdeli/enums/OrderStatus.kt
+++ app/src/main/java/com/example/dormdeli/enums/OrderStatus.kt
@@ -3,6 +3,8 @@ package com.example.dormdeli.enums
 enum class OrderStatus(val value: String) {
     PENDING("pending"),
     CONFIRMED("confirmed"),
+    ACCEPTED("accepted"),
+    PICKED_UP("picked_up"),
     DELIVERING("delivering"),
     COMPLETED("completed"),
     CANCELED("canceled");
